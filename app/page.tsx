a235f6cc2655da100814b71d4be6e313fe1e168d
isb <jaredlamont90@gmail.com>
2025-09-12 00:10:09 -0700

    Fixed: desktop spacing inconsistency

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
     <div className="">
       <main className="flex flex-col">
         {/* empty for now */}
-        <p className="pl-8 mt-10 w-[350px] tracking-wide underline underline-offset-8">I told my therapist about my trust issues.</p>
+        <p className="pl-8 sm:pl-10 mt-10 w-[350px] tracking-wide underline underline-offset-8">I told my therapist about my trust issues.</p>
       </main>
     </div>
   );

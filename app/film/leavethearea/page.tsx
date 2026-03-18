cfce497addb2eff0031449f4c6836d2d94bcff5d
isb <jaredlamont90@gmail.com>
2026-03-18 12:25:54 -0700

    Added: new page for leavethearea

---
+++ app/film/leavethearea/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Link from 'next/link';
+import Image from 'next/image';
+
+export default function LeaveTheArea() {
+  return (
+    <div className="pl-8 sm:pl-10 pt-10">
+      <div className="font-semibold text-lg">Leave The Area</div>
+    </div>
+  );
+}

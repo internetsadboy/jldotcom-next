aa31d8f6a0a8e877a9672c55fe8fd4fb4d4fc4c6
isb <jaredlamont90@gmail.com>
2026-03-18 12:50:04 -0700

    LTA: text outline for page

--- app/film/leavethearea/page.tsx
+++ app/film/leavethearea/page.tsx
@@ -7,6 +7,13 @@ export default function LeaveTheArea() {
   return (
     <div className="pl-8 sm:pl-10 pt-10">
       <div className="font-semibold text-lg">Leave The Area</div>
+      <ul>
+        <li>Stills</li>
+        <li>Vimeo Embed, customized display</li>
+        <li>Cannes Selection 2026</li>
+        <li>Logline</li>
+        <li>MAY PREMIERE</li>
+      </ul>
     </div>
   );
 }
